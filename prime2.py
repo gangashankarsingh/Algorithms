948d04d7e5fea6791248fc6b0fbd952c6f9bd486
System Administrator <root@Shunyam.local>
2019-08-05 22:25:53 +0530

    prime algo 2

---
+++ prime2.py
@@ -0,0 +1,19 @@
+import time
+from math import sqrt
+
+
+def checkprime(num):
+
+    for i in range(2, int(sqrt(num/2)) + 1):
+        if (num % i == 0):
+            return False
+    return True
+
+num=2
+start = int((time.time()))
+while True:
+    stop = int(time.time())-start
+    if checkprime(num):
+        if stop < 11:
+            print("Prime Number is {}. Total time taken so far is {}".format(num,stop))
+    num=num+1
